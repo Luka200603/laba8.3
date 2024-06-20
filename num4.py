02ec9c142f5b3283dc9ea747da0118a486885b00
Luka200603 <158995662+Luka200603@users.noreply.github.com>
2024-06-20 12:55:09 +0300

    Add files via upload

---
+++ num4.py
@@ -0,0 +1,4 @@
+def calc_stat(listened):  # от англ. calculate statistics, посчитать статистику
+        return (f'Вы прослушали {len(listened)} песен.') # напишите код функции calc_stat
+        
+print(calc_stat([193, 148, 210, 144, 174, 159, 163, 189, 230, 204]))
